e1a0137a72e6d744156c747d4e90074329efc7f4
chytran <chytran@users.noreply.github.com>
2021-06-04 15:17:48 -0700

    Understanding socket.io

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const io = require('socket.io')(3000);
+var express = require('express');
+var app = express();
+
+var cors = require('cors')
+
+app.use(cors());
 
 io.on('connection', socket => {
+    console.log("hello");
     socket.emit('chat-message', 'Hello World')
 });
