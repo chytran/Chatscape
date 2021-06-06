09ec3862355cedd74e150db7726ee7305991fd27
chytran <chytran@users.noreply.github.com>
2021-06-06 12:54:52 -0700

    Getting socket to work on chat page

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'assets/html')));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log("New connection");
+})
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
