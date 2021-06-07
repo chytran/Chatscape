6318d1966471e62d076cf183460122023c58adda
chytran <chytran@users.noreply.github.com>
2021-06-07 13:01:09 -0700

    Add messages in chat box. Clear input and focus

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ io.on('connection', socket => {
         io.emit('message', 'A user has left the chat');
     });
 
+    // listen for chatMessage
+    socket.on('chatMessage', (message) => {
+        io.emit('message', message);
+    })
 })
 
 const PORT = 3000 || process.env.PORT;
