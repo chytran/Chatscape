55237d0478a4ab2bdc5465614c2368b1b5edeb8d
chytran <chytran@users.noreply.github.com>
2021-06-08 11:48:44 -0700

    Setting up user names and rooms

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const moment = require('moment');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,26 +11,36 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'assets')));
 
+const botName = 'Chatscape bot';
+
 // Run when client connects
 io.on('connection', socket => {
-    console.log("New connection");
+    socket.on('joinRoom', ({ username, room}) => {
 
-    // broadcast to current user
-    socket.emit('message', "Welcome to the chat room");
+        const user = joinUser(socket.id, username, room);
 
-    // Broadcast when a user connects
-    // Broad to everyone except the client that is connecting
-    socket.broadcast.emit('message', 'A user has joined the chat');
+        socket.join(user.room);
 
-    // Runs when client disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        // broadcast to current user
+        socket.emit('message', formatMessage(botName, "Welcome to the chat room"));
+
+        // Broadcast when a user connects
+        // Broad to everyone except the client that is connecting
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
     });
+
+    console.log("New connection");
 
     // listen for chatMessage
     socket.on('chatMessage', (message) => {
-        io.emit('message', message);
+        io.emit('message', formatMessage('USER', message));
     })
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    });
 })
 
 const PORT = 3000 || process.env.PORT;
@@ -37,3 +48,29 @@ const PORT = 3000 || process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+function formatMessage(username, text) {
+    return {
+        username,
+        text,
+        time: moment().format('h:mm a')
+    }
+}
+
+/*========================================================================= */
+// User js
+const users = [];
+
+// join user to chat
+function joinUser(id, username, room){
+    const user = { id, username, room };
+
+    users.push(user);
+
+    return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+    return users.find(user => user.id === id);
+}
